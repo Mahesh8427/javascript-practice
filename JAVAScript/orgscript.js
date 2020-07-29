145ce9d3b9249187794fb47e4296375789778955
mahesh <kashyapmahesh128@gmail.com>
2020-07-29 18:43:09 +0530

    prototype

--- JAVAScript/orgscript.js
+++ JAVAScript/orgscript.js
@@ -37,7 +37,7 @@ y(x);
 // console.log ("program is started")
 
 // fs.readFile('input.txt', function  (err,data,callback) {
-//    if (err) {
+//    if(err) {
 //     return console.error(err);
 //    }else{
 //     console.log(data.toString());
@@ -57,28 +57,72 @@ message(function(check){ if(check){console.log("this age is allow")}else{console
 
 
 */
-var fs = require("fs");
+// call back function  
+// var fs = require("fs");
 
-console.log("program is started")
+// console.log("program is started")
 
-function asd(callback) {
-  fs.readFile('input.txt', function (err, data) {
-    if (err) {
-      return console.error(err);
-    } else {
-      console.log(data.toString());
-      callback()
-    }
-  });
+// function asd(callback) {
+//   fs.readFile('input.txt', function (err, data) {
+//     if(err) {
+//       return console.error(err);
+//     } else {
+//       console.log(data.toString());
+//       callback()
+//     }
+//   });
+// }
+
+
+
+// asd(function () {
+//     console.log("Program Ended");
+//   });
+
+//Again callback function with Arrow 
+
+
+// function sak(callback) {
+//   var name = " Ginvocining company ";
+//   callback(name);
+// }
+// sak((a) => {
+//   console.log(" please tell me  what is Your company name " + a + " ohk ");
+// console.log(" tell me about work in  your comapany ")});
+
+
+// javascript prototype 
+
+
+
+function mahesh(first, last, age, eye) {
+  this.f = first;
+  this.l = last;
+  this.a = age;
+  this.e = eye;
 }
 
-
-
-asd(function () {
-    console.log("Program Ended");
-  });
+mahesh.prototype.nationality = "English";
 
 
 
+function kashyap(first, last, age, eye){
+
+  mahesh.call(this, first, last, age, eye)
+
+}
+//prototype inheritance
+
+kashyap.prototype = Object.create(mahesh.prototype);
+
+//kashyap.prototype.constructor = kashyap;
+//kashyap.prototype.nationality = "hindi";
+
+var mykashyap = new kashyap("mahesh", "mak", 325, "white");
+console.log(mykashyap.f);
+console.log(mykashyap.l);
+console.log(mykashyap.a);
+console.log(mykashyap.e);
+console.log(mykashyap.nationality);
 
 

8018dee6928cdd4aff2e3d0e092b4e8b6d9e4194
mahesh <kashyapmahesh128@gmail.com>
2020-09-01 18:58:14 +0530

    Project2 Incomplete sept1

--- JAVAScript/project2.js
+++ JAVAScript/project2.js
@@ -1,21 +1,47 @@
 class Company {
     name;
+    
     departments = []
+    employees = []
     constructor(name) {
         this.name = name;
     }
-    
+
     addDepartment(add) {
         this.departments.push(add)
     }
+    totalEmployees() {
+        var sum = 0;
+        for (var i = 0; i < this.departments.length; i++) {
+
+            sum += parseInt(this.departments[i].employees.length)
+
+        }
+        console.log("Total no of empoloyees in companies are:- " + sum);
+    }
+
+    // deptPosition() {
+    //     for (var i = 0; i < this.departments.length; i++) {
+
+    //         if (this.departments.employess.Position === "Senoir") {
+    //             console.log("this Employee is " + this.departments.Position)
+    //         }else{
+    //             console.log("this Employee is " + this.departments.Position)
+    //         }
+
+
+    //     }
+    // }
+
+
+
 
 
 }
 
 class Departments {
     name;
-    employees = [];
-
+    employees = []
     constructor(name) {
         this.name = name;
     }
@@ -23,7 +49,11 @@ class Departments {
     addEmp(add) {
         this.employees.push(add)
     }
+
+
+
 }
+
 let employeeObjects = [{ name: "Raman", ESI: 320, Attendence: 30, PF: 1000, Departments: "Accounting", BaseSalary: 20000, MonthlySalary: 20000, Position: "Senior", SalaryPay: 18680 },
 { name: "Deepak", ESI: 224, Attendence: 28, PF: 0, Departments: "Development", BaseSalary: 15000, MonthlySalary: 14000, Position: "Junior", SalaryPay: 13776 },
 { name: "Rahul", ESI: 152.53, Attendence: 22, PF: 0, Departments: "Designing", BaseSalary: 13000, MonthlySalary: 9533.33, Position: "Senior", SalaryPay: 9380.8 },
@@ -32,7 +62,7 @@ let employeeObjects = [{ name: "Raman", ESI: 320, Attendence: 30, PF: 1000, Depa
 { name: "Nisha", ESI: 384, Attendence: 24, PF: 1200, Departments: "Accounting", BaseSalary: 30000, MonthlySalary: 24000, Position: "Senior", SalaryPay: 22416 },
 { name: "Jack", ESI: 398.93, Attendence: 22, PF: 1246.67, Departments: "HR", BaseSalary: 34000, MonthlySalary: 24933.33, Position: "Senior", SalaryPay: 23287.73 },
 { name: "Mak", ESI: 316.8, Attendence: 27, PF: 990, Departments: "Development", BaseSalary: 22000, MonthlySalary: 19800, Position: "Senior", SalaryPay: 18493.2 },
-{ name: "Rohit", ESI: 268.8, Attendence: 21, PF: 840, Departments: "Development", BaseSalary: 24000, MonthlySalary: 16800, Position: "Junior", SalaryPay: 15691.2 },
+{ name: "Rohit", ESI: 268.8, Attendence: 21, PF: 840, Departments: "ABCDevops", BaseSalary: 24000, MonthlySalary: 16800, Position: "Junior", SalaryPay: 15691.2 },
 { name: "Maki", ESI: 277.33, Attendence: 20, PF: 866.67, Departments: "Accounting", BaseSalary: 26000, MonthlySalary: 17333.33, Position: "Junior", SalaryPay: 16189.33 }]
 
 let companyObjects = new Company("ABCDevOps")
@@ -46,14 +76,14 @@ var hr = new Departments("HR")
 for (var i = 0; i < employeeObjects.length; i++) {
 
     if (employeeObjects[i].Departments == "Accounting") {
-        accounts.addEmp(Objects[i])
+        accounts.addEmp(employeeObjects[i])
     }
     else if (employeeObjects[i].Departments == "Development") {
-        development.addEmp(Objects[i])
+        development.addEmp(employeeObjects[i])
     } else if (employeeObjects[i].Departments == "Designing") {
-        design.addEmp(Objects[i])
+        design.addEmp(employeeObjects[i])
     } else if (employeeObjects[i].Departments == "ABCDevops") {
-        devops.addEmp(Objects[i])
+        devops.addEmp(employeeObjects[i])
     } else if (employeeObjects[i].Departments == "HR") {
         hr.addEmp(employeeObjects[i])
     } else {
@@ -61,16 +91,21 @@ for (var i = 0; i < employeeObjects.length; i++) {
     }
 }
 
-// console.log(accounts);
-// console.log(development);
 
-
+//console.log(development);
+//console.log(design);
+//console.log(deveops);
+//console.log(hr);
 companyObjects.addDepartment(accounts);
 companyObjects.addDepartment(development);
 companyObjects.addDepartment(design);
 companyObjects.addDepartment(devops);
 companyObjects.addDepartment(hr);
 
+companyObjects.totalEmployees();
+//companyObjects.deptPosition();
+
+console.log(companyObjects.departments[1])
 
 class EmployeeDetails {
     name;
@@ -78,16 +113,15 @@ class EmployeeDetails {
     department;
     salary;
     position;
-    constructor(name, employeesid, department, attendence, position, salary) {
+    constructor(name, employeesid, departments, attendence, position, salary) {
         this.name = name;
         this.employeesid = employeesid;
-        this.department = department;
+        this.departments = departments;
         this.position = position;
         this.salary = salary;
         this.attendence = attendence;
     }
 }
-
 
 
 

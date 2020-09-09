39535f5ec38f405373946dc160e87bf8d10c45b0
mahesh <kashyapmahesh128@gmail.com>
2020-09-09 19:11:44 +0530

    Project3 Incomplete sept9

--- JAVAScript/callbackfunction.js
+++ JAVAScript/callbackfunction.js
@@ -1,76 +1,76 @@
-//var fs = require("fs");
+// //var fs = require("fs");
 
 
-let x = function () {
-  console.log("Company is  GinVocining");
-}
-let y = function (callback) {
-  console.log("wht is company name");
-  callback();
-}
-y(x);
-
-
-
-//  Again simple callback Function..(2)
-
-function show(a) {
-  console.log("I am show function" + a)
-}
-function geeky(callback) {
-  var s = 1;
-  callback(s);
-}
-geeky(show);
-
-// console.log("end");
-
-//Asynchrnous way perfrom program..
-var fs = require("fs");
-console.log("program is started")
-
-
-fs.readFile('input.txt', function (err, data) {
-  if (err) {
-    return console.error(err);
-  } else {
-    console.log(data.toString());
-
-  }
-});
-
-console.log("Program Ended");
-
-
-//call back function with anonymous Function..(3)  
-var fs = require("fs");
-
-console.log("program is started")
-
-function asd(callback) {
-  fs.readFile('input.txt', function (err, data) {
-    if (err) {
-      return console.error(err);
-    } else {
-      console.log(data.toString());
-      callback()
-    }
-  });
-}
-asd(function () {                  // anonymous function
-  console.log("Program Ended");
-});
+// let x = function () {
+//   console.log("Company is  GinVocining");
+// }
+// let y = function (callback) {
+//   console.log("wht is company name");
+//   callback();
+// }
+// y(x);
 
 
 
+// //  Again simple callback Function..(2)
+
+// function show(a) {
+//   console.log("I am show function" + a)
+// }
+// function geeky(callback) {
+//   var s = 1;
+//   callback(s);
+// }
+// geeky(show);
+
+// // console.log("end");
+
+// //Asynchrnous way perfrom program..
+// var fs = require("fs");
+// console.log("program is started")
 
 
-//CallBack Function with Arrow Function..(4)
-function sak(callback) {
-  var name = " Ginvocining company ";
-  callback(name);
-}
-sak((a) => { console.log(" please tell me  what is Your company name " + a + " ohk ") });         // <=arow function      
+// fs.readFile('input.txt', function (err, data) {
+//   if (err) {
+//     return console.error(err);
+//   } else {
+//     console.log(data.toString());
+
+//   }
+// });
+
+// console.log("Program Ended");
+
+
+// //call back function with anonymous Function..(3)  
+// var fs = require("fs");
+
+// console.log("program is started")
+
+// function asd(callback) {
+//   fs.readFile('input.txt', function (err, data) {
+//     if (err) {
+//       return console.error(err);
+//     } else {
+//       console.log(data.toString());
+//       callback()
+//     }
+//   });
+// }
+// asd(function () {                  // anonymous function
+//   console.log("Program Ended");
+// });
+
+
+
+
+
+// //CallBack Function with Arrow Function..(4)
+// function sak(callback) {
+//   var name = " Ginvocining company ";
+//   callback(name);
+// }
+// sak((a) => { console.log(" please tell me  what is Your company name " + a + " ohk ") });         // <=arow function      
 
 
 
--- JAVAScript/import.js
+++ JAVAScript/import.js
@@ -1,4 +1,4 @@
-
+ 
 var mahesh = require('./export');
 
 // var a = "male";
